9f1a97ddf6a22bed2fc1fd35ceef3d2d7e65e746
Krisma Antonio <krismapaulene.antonio681@myci.csuci.edu>
2025-11-24 21:26:36 -0800

    deleted Main.jsx and move it to App.jsx

--- personal-website/src/App.jsx
+++ personal-website/src/App.jsx
@@ -1,10 +1,32 @@
-import Main from './components/Main';
+import Introduction from "./components/Introduction"
+import About from "./components/About"
+import Projects from "./components/Projects"
 
 function App() {
 
   return (
+   
      <>
-     <Main/>
+        <header>
+            <ul className="header-container">
+                <li className="leftHeader"><a className="text-header roboto-title" href="#">Logo</a></li>
+                <li className="rightHeader"><a className="text-header roboto-title" href="#projects">Projects</a></li>
+                <li className="rightHeader"><a className="text-header roboto-title" href="#about">About</a></li>
+            </ul>
+        </header>
+
+        <div className="row-intro">
+            <Introduction/>
+        </div>
+
+        <div id="about" className="about-body">
+            <About/>
+        </div>
+
+        <div id="projects" className="project-body">
+            <Projects/>
+        </div>
+    
     </>
   )
 }
